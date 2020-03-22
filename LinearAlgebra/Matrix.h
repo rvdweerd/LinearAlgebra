e640a189d4880c671d5f66c3b635bc5161384025
RogVDW <rogerscode@gmail.com>
2020-03-22 16:53:22 +0100

    elimination matrices routines added

--- LinearAlgebra/Matrix.h
+++ LinearAlgebra/Matrix.h
@@ -9,14 +9,16 @@ namespace LinA
 	{
 	public:
 		Matrix(std::string str);
+		Matrix(int m, int n, float val);
 		Matrix()
 		{
 			std::cout << "default ctor\n";
 		}// = default;
 		Matrix Transpose();
-		void RowOp(int multiplier, int subtract, int into);
+		void RowOp(float multiplier, int subtract, int into);
 		void RowExchange(int row1, int row2);
-	private:
+		Matrix RowReduce(int col);
+	public:
 		std::vector<std::vector<float>> A;
 		size_t m = 0; // number of rows
 		size_t n = 0; // number of columns
@@ -24,11 +26,13 @@ namespace LinA
 		
 		friend std::ostream& operator<<(std::ostream& stream, LinA::Matrix A);
 		friend Matrix operator*(LinA::Matrix lhs, LinA::Matrix rhs);
-		friend void RowOp(Matrix& A, int multiplier, int subtract, int into);
+		friend void RowOp(Matrix& A, float multiplier, int subtract, int into);
 		friend void RowExchange(Matrix& A, int row1, int row2);
 	};
 
-	void RowOp(Matrix& A, int multiplier, int subtract, int into);
+	static Matrix Zeros(int m, int n);
+	static Matrix Eye(int n);
+	void RowOp(Matrix& A, float multiplier, int subtract, int into);
 	void RowExchange(Matrix& A, int row1, int row2);
 }
 
